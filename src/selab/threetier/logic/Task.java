ea21609663d37e28a5bddbcd6a82a66b1a85937f
Mahdi Hasanzadeh <mahdihasanzadeh76@gmail.com>
2020-08-06 22:08:36 +0430

    ThreeTier 2. Validate added cards to do not overlap and the start time not being after the end time.

--- src/selab/threetier/logic/Task.java
+++ src/selab/threetier/logic/Task.java
@@ -1,7 +1,9 @@
 package selab.threetier.logic;
 
+import selab.threetier.storage.EntityStorage;
 import selab.threetier.storage.Storage;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,24 +13,47 @@ public class Task extends Entity {
     private Date start;
     private Date end;
 
-    public String getTitle() { return title; }
-    public void setTitle(String value) { title = value; }
+    public String getTitle() {
+        return title;
+    }
 
-    public void setStart(Date value) { start = value; }
+    public void setTitle(String value) {
+        title = value;
+    }
+
+    public void setStart(Date value) {
+        start = value;
+    }
+
     public String getStartDate() {
         return new SimpleDateFormat("YYYY-MM-DD").format(start);
     }
+
     public String getStartTime() {
         return new SimpleDateFormat("HH:mm:ss").format(start);
     }
 
-    public void setEnd(Date value) { end = value; }
+    public void setEnd(Date value) {
+        end = value;
+    }
+
     public String getEndTime() {
         return new SimpleDateFormat("HH:mm:ss").format(end);
     }
 
-    public void save() {
-        Storage.getInstance().getTasks().addOrUpdate(this);
+    public void save() throws IOException {
+        if (start.after(end)) {
+            throw new IOException("Start time can not set after end time");
+        }
+        EntityStorage<Task> tasks = Storage.getInstance().getTasks();
+        boolean existOverlap = tasks.getAll()
+                .stream()
+                .anyMatch(task -> ((task.start.after(start) || task.start.equals(start)) && task.start.before(end))
+                        || ((task.end.before(end) || task.end.equals(end)) && task.end.after(start)));
+        if (existOverlap) {
+            throw new IOException("Overlap task added");
+        }
+        tasks.addOrUpdate(this);
     }
 
     public static ArrayList<Task> getAll() {
